852e9ec080adb5464e4d63f39f76c61186884331
meghanlu <lumeghan@gmail.com>
2021-07-12 23:54:53 -0500

    Added todo list functionality

--- bot.py
+++ bot.py
@@ -12,11 +12,18 @@ wa_client = wolframalpha.Client(appid)
 
 mjs = MathJS()
 
+todo_list = []
+
 bot = commands.Bot(command_prefix=('homie ', '!'), case_insensitive=True)
 
 @bot.event
 async def on_message(message):
     if message.author.bot:
+        # Check if message is by another bot or itself
+        if (message.author == bot.user and 
+        re.match('^`\d+\.\s',message.content)):
+            # Add reaction to todo list
+            await message.add_reaction('✅')
         return
 
     if message.content.lower().startswith("solve"):
@@ -32,6 +39,21 @@ async def on_message(message):
         await message.channel.send(answer)
 
     await bot.process_commands(message)
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    if user.bot:
+        return
+    
+    if reaction.emoji == '✅' and  reaction.message.author == bot.user:
+        # If someone checked something off of the todo list
+        todo_item = re.sub('^`\d+\.\s', '', reaction.message.content)
+        todo_item = todo_item[:len(todo_item) - 1]
+        if todo_item != reaction.message.content and todo_item in todo_list:
+            todo_list.remove(todo_item)
+            await reaction.message.add_reaction('🎉')
+            await reaction.message.channel.send(("Congrats on finishing " + 
+            todo_item + "!!!"))
 
 def make_codeblock(string : str):
     return "`" + string + "`"
